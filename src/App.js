abaa238568bbf0447f7aa6d73a240c254cccd138
Luca Cavretti <cavretiluca@live.it>
2021-02-24 20:21:33 +0100

    added sign in and sign out page, added sign in component as well as button component and form component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from "./components/header/header.component.jsx";
+import SignInAndSignOutPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
+import Header from "./components/header/header.component";
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignOutPage} />
       </Switch>
     </div>
   );
